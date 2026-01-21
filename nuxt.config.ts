231cffaef11c82d482dcf306071ed5e118127cb4
Soma Iwasaki(Desktop) <somanosuke.suke@gmail.com>
2026-01-22 00:05:36 +0900

    workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   nitro: {
-    preset: "cloudflare",
+    preset: 'cloudflare_module',
+   cloudflare: {
+     deployConfig: true,
+     nodeCompat: true,
+   },
   },
 });
